973c09ef825b21ee594ebe6163eb610b9711a3ae
nolte <nolte07@googlemail.com>
2019-02-09 20:55:11 +0100

    prepare gh page

--- Jenkinsfile
+++
@@ -1,8 +0,0 @@
-#!groovy
-
-@Library('gitflow@develop')
-import de.noltarium.jenkins.gitflow.*;
-
-python3Build {
-    echo "foooo"
-}
--- gitlab_bulkcheckout/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for project manager."""
-
-__author__ = """nolte"""
-__version__ = "0.1.0"
