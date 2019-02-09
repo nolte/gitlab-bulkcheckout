bda28d01a830a28c94c4f6ff976573681670ce29
nolte <nolte07@googlemail.com>
2019-02-09 20:23:46 +0000

    Bump version: 0.2.1 → 0.3.0-dev

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/nolte/project_manager",
-    version="version='0.2.1'",
+    version="version='0.3.0-dev'",
     zip_safe=False,
 )
