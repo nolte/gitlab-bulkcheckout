6d201112c3b890398a0c55b4c1d413c2d1e76b3b
nolte <nolte07@googlemail.com>
2019-02-10 17:44:13 +0100

    add manifest file

--- setup.py
+++ setup.py
@@ -3,10 +3,22 @@
 
 """The setup script."""
 
+
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+from os import path
+
+# Always prefer setuptools over distutils
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as fh:
-    readme = fh.read()
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 
 requirements = ["Click==7.0", "pyyaml>=4.2b1", "python-gitlab==1.7.0", "gitpython==2.1.11"]
@@ -32,16 +44,21 @@ setup(
     entry_points={"console_scripts": ["gitlab_bulkcheckout=gitlab_bulkcheckout.cli:main"]},
     install_requires=requirements,
     license="Apache Software License 2.0",
-    long_description=readme,
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    include_package_data=True,
+    # include_package_data=True,
+    # exclude_package_data={"": ["Dockerfile"], "": ["docs"], "": [".travis.yml"]},
     keywords="gitlab_bulkcheckout",
     name="gitlab_bulkcheckout",
-    packages=find_packages(include=["gitlab_bulkcheckout"]),
+    # package_dir={"": "gitlab_bulkcheckout"},
+    packages=find_packages(
+        include=["gitlab_bulkcheckout"],
+        # exclude=["*.circleci", "*Dockerfile", "*.travisci", "*.tests", "*.tests.*", "tests.*", "tests"],
+    ),
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/nolte/gitlab-bulkcheckout",
     version="0.22.0-dev",
-    zip_safe=False,
+    # zip_safe=False,
 )
